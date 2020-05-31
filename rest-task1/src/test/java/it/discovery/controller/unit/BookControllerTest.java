6323733110e1dd3b6a75dc24122585831669b947
Sergiy Morenets <sergey.morenets@gmail.com>
2020-05-31 15:50:06 +0300

    Task #7

--- rest-task1/src/test/java/it/discovery/controller/unit/BookControllerTest.java
+++ rest-task1/src/test/java/it/discovery/controller/unit/BookControllerTest.java
@@ -6,6 +6,7 @@ import it.discovery.model.Book;
 import it.discovery.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -13,16 +14,18 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitWebConfig(RestApplication.class)
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 public class BookControllerTest {
     @Autowired
@@ -44,7 +47,12 @@ public class BookControllerTest {
 
         actions.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(1)));
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andDo(document("{class-name}/{method-name}", responseFields(fieldWithPath("[].id")
+                                .description("Book unique identifier"),
+                        fieldWithPath("[].author").description("Book author"),
+                        fieldWithPath("[].title").description("Book title"),
+                        fieldWithPath("[].year").description("Book publishing year"))));
     }
 
     @Test
